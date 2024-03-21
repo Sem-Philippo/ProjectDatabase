ac0b070a3358010fa423b10414567df5c09f512b
redroli2 <papproland59@gmail.com>
2024-03-21 12:10:22 +0100

    SomerenUI

--- SomerenUI/SomerenUI.cs
+++ SomerenUI/SomerenUI.cs
@@ -3,7 +3,6 @@ using SomerenModel;
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System;
-using System.Drawing;
 
 namespace SomerenUI
 {
@@ -17,7 +16,7 @@ namespace SomerenUI
         {
             pnlStudents.Hide();
             pnlDashboard.Hide();
-            //RoomPanel here
+            RoomsPanel.Hide();
             //LecturerPanel here
             //activity panel here
             //drink panel here
@@ -26,16 +25,62 @@ namespace SomerenUI
 
         private void ShowDashboardPanel()
         {
-            HideAllPanels();
+            // hide all other panels
+            pnlStudents.Hide();
 
             // show dashboard
             pnlDashboard.Show();
         }
+        private void DisplayRooms(List<Room> rooms)
+        {
+            // clear the listview before filling it
+            listViewRooms.Items.Clear();
 
+            foreach (Room room in rooms)
+            {
+                // Determine the room type based on the number of beds
+                string roomType = room.Beds > 1 ? "Student" : "Lecturer";
+
+                // Include the room type in the subItems array
+                string[] subItems = new string[5] { room.Number.ToString(), room.Floor.ToString(), room.Building.ToString(), room.Beds.ToString(), roomType };
+
+                // Create a new ListViewItem with the subItems array
+                ListViewItem li = new ListViewItem(subItems);
+                li.Tag = room;   // link room object to listview item
+
+                // Add the new ListViewItem to the listViewRooms
+                listViewRooms.Items.Add(li);
+            }
+
+        }
+        private List<Room> GetRooms()
+        {
+            RoomService roomService = new RoomService();
+            List<Room> rooms = roomService.GetRooms();
+            return rooms;
+        }
+        private void ShowRoomsPanel()
+        {
+            HideAllPanels();
+
+            RoomsPanel.Show();
+
+            try
+            {
+                // get and display all rooms
+                List<Room> room = GetRooms();
+                DisplayRooms(room);
+                //  RoomsPanel.Show();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong while loading the rooms: " + e.Message);
+            }
+        }
         private void ShowStudentsPanel()
         {
             // hide all other panels
-            HideAllPanels();
+            pnlDashboard.Hide();
 
             // show students
             pnlStudents.Show();
@@ -62,12 +107,11 @@ namespace SomerenUI
         private void DisplayStudents(List<Student> students)
         {
             // clear the listview before filling it
-            listViewStudents.Items.Clear();
+            listViewStudents.Clear();
 
             foreach (Student student in students)
             {
-                string[] subItems = new string[5] { student.Name, student.Number.ToString(), student.Class, student.PhoneNumber, student.RoomNumber.ToString() };
-                ListViewItem li = new ListViewItem(subItems);
+                ListViewItem li = new ListViewItem(student.Name);
                 li.Tag = student;   // link student object to listview item
                 listViewStudents.Items.Add(li);
             }
@@ -76,6 +120,7 @@ namespace SomerenUI
         private void dashboardToolStripMenuItem1_Click(object sender, System.EventArgs e)
         {
             ShowDashboardPanel();
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, System.EventArgs e)
@@ -86,6 +131,11 @@ namespace SomerenUI
         private void studentsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             ShowStudentsPanel();
+        }
+
+        private void roomsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowRoomsPanel();
         }
     }
 }
